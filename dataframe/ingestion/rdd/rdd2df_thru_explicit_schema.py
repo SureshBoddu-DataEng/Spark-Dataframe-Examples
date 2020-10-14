6e5a57139d5467fdb7b7ff5c7d5103e30943d6c3
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2020-10-14 19:49:39 +0530

    changes

--- dataframe/ingestion/rdd/rdd2df_thru_explicit_schema.py
+++ dataframe/ingestion/rdd/rdd2df_thru_explicit_schema.py
@@ -11,8 +11,7 @@ if __name__ == '__main__':
     spark = SparkSession \
         .builder \
         .appName("DataFrames examples") \
-        #.config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.4') \
-        .master('local[*]') \
+        .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.4') \
         .getOrCreate()
 
     spark.sparkContext.setLogLevel("ERROR")
