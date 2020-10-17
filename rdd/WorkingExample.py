ba457836f671db8f8ef39c8fe4aa2c6ca5581ecf
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2020-10-17 16:21:52 +0530

    my program

--- rdd/WorkingExample.py
+++ rdd/WorkingExample.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
         .builder \
         .appName("Spark Dataframe example") \
         .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.4') \
-        .master('local[*]') \
         .getOrCreate()
 
+    spark.sparkContext.setLogLevel("ERROR")
 # Second Step : set diretory and paths
 
     current_dir = os.path.abspath(os.path.dirname(__file__))
@@ -40,4 +40,4 @@ if __name__ == "__main__":
 
     demographics_pair_rdd.foreach(print)
 
-#  spark-submit --master yarn --packages "org.apache.hadoop:hadoop-aws:2.7.4" rdd/WorkingExample.py
+#  spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" rdd/WorkingExample.py
