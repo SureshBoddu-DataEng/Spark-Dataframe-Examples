a292b70e2cfe0661df15b11b74322f06a55cc691
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2020-10-17 16:09:18 +0530

    my program

--- rdd/WorkingExample.py
+++ rdd/WorkingExample.py
@@ -31,22 +31,12 @@ if __name__ == "__main__":
     hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
 
     demographics_rdd = spark.sparkContext.textFile("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/demographic.csv")
-    finances_rdd = spark.sparkContext.textFile("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/finances.csv")
 
     demographics_pair_rdd = demographics_rdd \
         .map(lambda line: line.split(",")) \
         .map(lambda lst: (
     int(lst[0]), (int(lst[1]), strtobool(lst[2]), lst[3], lst[4], strtobool(lst[5]), strtobool(lst[6]), int(lst[7]))))
 
-    finances_pair_rdd = finances_rdd \
-        .map(lambda line: line.split(",")) \
-        .map(lambda lst: (int(lst[0]), (strtobool(lst[1]), strtobool(lst[2]), strtobool(lst[3]), int(lst[4]))))
-
-    print('Participants belongs to \'Switzerland\', having debts and financial dependents,')
-    join_pair_rdd = demographics_pair_rdd \
-        .join(finances_pair_rdd) \
-        .filter(lambda rec: (rec[1][0][2] == "Switzerland") and (rec[1][1][0] == 1) and (rec[1][1][1] == 1)) \
- \
-            join_pair_rdd.foreach(print)
+    demographics_pair_rdd.foreach(print)
 
 #  spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" rdd/WorkingExample.py
