c5dc4c4c847a82eeabf7344aa36c8c3263cf0ec3
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2020-10-20 16:10:24 +0530

    changes

--- dataframe/ingestion/others/systems/mysql_df.py
+++ dataframe/ingestion/others/systems/mysql_df.py
@@ -6,7 +6,7 @@ def get_mysql_jdbc_url(mysql_config: dict):
     host = mysql_config["mysql_conf"]["hostname"]
     port = mysql_config["mysql_conf"]["port"]
     database = mysql_config["mysql_conf"]["database"]
-    return "jdbc:mysql://{}:{}?autoReconnect=true&useSSL=false".format(host, port)
+    return "jdbc:mysql://{}:{}/{}?autoReconnect=true&useSSL=false".format(host, port, database)
 
 if __name__ == '__main__':
 
