be175802358f3b74fe6a1aef3511170609200ac1
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2020-10-24 18:44:59 +0530

    changes

--- dataframe/provision/df_mongo.py
+++ dataframe/provision/df_mongo.py
@@ -1,7 +1,15 @@
 from pyspark.sql import SparkSession
 import yaml
 import os.path
-from model.Student import Student
+#from model.Student import Student
+
+from dataclasses import dataclass
+@dataclass(frozen=True)
+class Student(object):
+    firstname: str
+    lastname: str
+    school: str
+    studentno: int
 
 if __name__ == '__main__':
 
